473fc445cf2ad43fd22fa14d4dadad6b6e0e1ef2
Ngireddy <somulanagireddy@gmail.com>
2021-03-25 07:18:48 +0530

    [Nagireddy]: added finding max number in float

--- src/main/java/Bridgelabz/FindMaxNumber.java
+++ src/main/java/Bridgelabz/FindMaxNumber.java
@@ -1,8 +1,16 @@
 package Bridgelabz;
 
 public class FindMaxNumber {
-    public Integer testMax(Integer A, Integer B, Integer C){
+    public Integer MaxTest(Integer A, Integer B, Integer C){
         int max = A;
+        if(B.compareTo(max) > 0)
+            max = B;
+        if(C.compareTo(max) > 0)
+            max = C;
+        return max;
+    }
+    public Float MaxTest(Float A, Float B, Float C){
+        Float max = A;
         if(B.compareTo(max) > 0)
             max = B;
         if(C.compareTo(max) > 0)
